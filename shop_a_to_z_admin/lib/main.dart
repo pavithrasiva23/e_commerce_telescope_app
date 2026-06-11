e17e8c65219a4275fd84675a3c6106863aef80be
Pavithra Sivakumar <pavithrasiva23@gmail.com>
2026-06-11 11:30:59 +0530

    Small changes 2

--- shop_a_to_z_admin/lib/main.dart
+++ shop_a_to_z_admin/lib/main.dart
@@ -22,7 +22,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
   //I removed 'Firebase.initializeApp() because already it initialized in the main() function
